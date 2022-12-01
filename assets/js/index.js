60a844a1811a946bcb02c49437d8568b9b8debf9
Arekkusu17 <fernn.alex@gmail.com>
2022-12-01 10:58:56 -0300

    update responsive design, clean unused code, add some styles

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,8 @@
-const tasks = [];
+const tasks = [
+  { id: 1, taskName: "Ir al supermercado", completed: false },
+  { id: 2, taskName: "Ordenar la habitación", completed: false },
+  { id: 3, taskName: "Realizar tarea del bootcamp", completed: false },
+];
 const inputTask = document.getElementById("newTask");
 const button = document.getElementById("addTask");
 const idList = document.getElementById("idList");
@@ -6,123 +10,7 @@ const taskNameList = document.getElementById("taskNameList");
 const checkDelete = document.getElementById("checkDelete");
 const total = document.getElementById("totalTask");
 const completedTask = document.getElementById("completedTask");
-let id = 0;
-
-button.addEventListener("click", (e) => {
-  e.preventDefault();
-  id += 1;
-  console.log("here");
-  const newTask = inputTask.value;
-  tasks.push({ id: id, taskName: newTask, completed: false });
-  inputTask.value = "";
-  console.log(tasks);
-
-  writeTasks(tasks);
-
-  //let newIdTask = "";
-  //let newNameTask = "";
-  //let newChecks = "";
-  //for (let task of tasks) {
-  //  let isChecked = "";
-  //  if (task.completed === true) {
-  //    isChecked = "checked";
-  //    newNameTask += `<li class="checked">${task.taskName}</li>`;
-  //  } else {
-  //    newNameTask += `<li>${task.taskName}</li>`;
-  //  }
-  //  newIdTask += `<li>${task.id}</li>`;
-  //  //newNameTask += `<li>${task.taskName}</li>`;
-  //  newChecks += `<div class="form-check">
-  //  <input onclick="updateTask(event,${task.id})" class="form-check-input" type="checkbox" value="" id="checkbox-${task.id}" ${isChecked}>
-  //  <label class="form-check-label" for="checkbox-${task.id}">
-  //  <p>|</p>
-  //  </label>
-  //  <a onclick="deleteTask(${task.id})">❌</a>
-  //</div>`;
-  //}
-  //total.textContent = tasks.length;
-  //idList.innerHTML = newIdTask;
-  //taskNameList.innerHTML = newNameTask;
-  //checkDelete.innerHTML = newChecks;
-});
-
-const deleteTask = (id) => {
-  const index = tasks.findIndex((element) => element.id == id);
-  if (tasks[index].completed) {
-    completedTask.textContent--;
-  }
-  tasks.splice(index, 1);
-
-  writeTasks(tasks);
-  //let newIdTask = "";
-  //let newNameTask = "";
-  //let newChecks = "";
-  //for (let task of tasks) {
-  //  let isChecked = "";
-  //  if (task.completed === true) {
-  //    isChecked = "checked";
-  //    newNameTask += `<li class="checked">${task.taskName}</li>`;
-  //  } else {
-  //    newNameTask += `<li>${task.taskName}</li>`;
-  //  }
-  //  newIdTask += `<li>${task.id}</li>`;
-  //  //newNameTask += `<li>${task.taskName}</li>`;
-  //  newChecks += `<div class="form-check">
-  //  <input onclick="updateTask(event,${task.id})" class="form-check-input" type="checkbox" value="" id="checkbox-${task.id}" ${isChecked}>
-  //  <label class="form-check-label" for="checkbox-${task.id}">
-  //  <p>|</p>
-  //  </label>
-  //  <a onclick="deleteTask(${task.id})">❌</a>
-  //</div>`;
-  //}
-  //total.textContent = tasks.length;
-  //idList.innerHTML = newIdTask;
-  //taskNameList.innerHTML = newNameTask;
-  //checkDelete.innerHTML = newChecks;
-};
-
-const updateTask = (event, id) => {
-  const checkbox = document.getElementById(`checkbox-${id}`);
-  const index = tasks.findIndex((element) => element.id == id);
-
-  //let doneText = "";
-  //let doneText = " || DONE";
-  if (checkbox.checked) {
-    tasks[index].completed = true;
-    //tasks[index].taskName = `${tasks[index].taskName}` + doneText;
-    completedTask.textContent++;
-  } else {
-    tasks[index].completed = false;
-    completedTask.textContent--;
-  }
-
-  writeTasks(tasks);
-  // let newIdTask = "";
-  // let newNameTask = "";
-  // let newChecks = "";
-  // for (let task of tasks) {
-  // let isChecked = "";
-  // if (task.completed === true) {
-  // isChecked = "checked";
-  // newNameTask += `<li class="checked">${task.taskName}</li>`;
-  // } else {
-  // newNameTask += `<li>${task.taskName}</li>`;
-  // }
-  // newIdTask += `<li>${task.id}</li>`;
-  //newNameTask += `<li>${task.taskName}</li>`;
-  // newChecks += `<div class="form-check">
-  // <input onclick="updateTask(event,${task.id})" class="form-check-input" type="checkbox" value="" id="checkbox-${task.id}" ${isChecked}>
-  // <label class="form-check-label" for="checkbox-${task.id}">
-  // <p>|</p>
-  // </label>
-  // <a onclick="deleteTask(${task.id})">❌</a>
-  // </div>`;
-  // }
-  // total.textContent = tasks.length;
-  // idList.innerHTML = newIdTask;
-  // taskNameList.innerHTML = newNameTask;
-  // checkDelete.innerHTML = newChecks;
-};
+let id = tasks.length;
 
 const writeTasks = (arr) => {
   let newIdTask = "";
@@ -154,4 +42,43 @@ const writeTasks = (arr) => {
   idList.innerHTML = newIdTask;
   taskNameList.innerHTML = newNameTask;
   checkDelete.innerHTML = newChecks;
+};
+
+writeTasks(tasks);
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  id += 1;
+  console.log("here");
+  const newTask = inputTask.value;
+  tasks.push({ id: id, taskName: newTask, completed: false });
+  inputTask.value = "";
+  console.log(tasks);
+
+  writeTasks(tasks);
+});
+
+const deleteTask = (id) => {
+  const index = tasks.findIndex((element) => element.id == id);
+  if (tasks[index].completed) {
+    completedTask.textContent--;
+  }
+  tasks.splice(index, 1);
+
+  writeTasks(tasks);
+};
+
+const updateTask = (event, id) => {
+  const checkbox = document.getElementById(`checkbox-${id}`);
+  const index = tasks.findIndex((element) => element.id == id);
+
+  if (checkbox.checked) {
+    tasks[index].completed = true;
+    completedTask.textContent++;
+  } else {
+    tasks[index].completed = false;
+    completedTask.textContent--;
+  }
+
+  writeTasks(tasks);
 };
